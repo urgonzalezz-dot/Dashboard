ac8e9fdee4d0ecc3f469d1741eec95d3e7176637
Rodrigo Gonzalez <urgonzalezz@liverpool.com.mx>
2026-01-16 10:59:30 -0600

    feat(ui): restore host context and MfeContainer

--- libs/ui/src/index.js
+++ libs/ui/src/index.js
@@ -2,3 +2,5 @@ export * from './lib/theme/theme';
 export * from './lib/components/Button/Button';
 export * from './lib/components/UpdateCard/UpdateCard';
 export * from './lib/components/GenericCard/GenericCard';
+export * from './lib/context';
+export * from './lib/components/MfeContainer';
