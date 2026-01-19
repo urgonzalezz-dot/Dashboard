83453ffd875afa1f335c6293f2434a73cd0f8b8b
Rodrigo Gonzalez <urgonzalezz@liverpool.com.mx>
2026-01-19 11:56:10 -0600

    feat(libs): add hooks and utilities base

---
+++ libs/utilities/src/lib/utilities.js
@@ -0,0 +1,11 @@
+import styles from './utilities.module.scss';
+
+export function Utilities() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Utilities!</h1>
+    </div>
+  );
+}
+
+export default Utilities;
