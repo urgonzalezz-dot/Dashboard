20fabf70c91d915df7fc7f2ffe9e964f56b98c9e
Rodrigo Gonzalez <urgonzalezz@liverpool.com.mx>
2026-01-21 10:06:45 -0600

    feat(host): mount redux provider and store

--- apps/host/src/bootstrap.js
+++ apps/host/src/bootstrap.js
@@ -1,12 +1,23 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+/* import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client'; */
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { createAppStore } from '@libs/redux';
+import FatalError from './components/FatalError/FatalError';
 import App from './app/app';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = createAppStore();
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <FatalError>
+        <App />
+      </FatalError>
+    </Provider>
+  </BrowserRouter>
 );
